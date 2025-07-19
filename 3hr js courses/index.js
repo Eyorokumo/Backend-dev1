6b5c7925e9e2ce3e9f0e7f675c2ed29db957e7cb
Eyorokumo <eyorokorumodicksonemmanuel@gmail.com>
2025-07-19 13:19:14 +0100

    fresh

---
+++ 3hr js courses/index.js
@@ -0,0 +1,256 @@
+        // Data Types:
+        // undefined,null,boolean,string,symbol,number, and object
+        // 
+//         var myName = "Beau"
+
+//         myName = 8
+
+//         let ourName = "freeCodeCamp"
+
+//         const pi = 3.14
+
+//         // Storing Values with Assignment Operator
+        
+//         var a;
+//         var b = 2;
+// console.log(a)
+//         a = 7;
+
+//         b = a;
+
+//         console.log(a)
+// 
+    // Initializing variables w/ Assighment operator
+    // 
+//     var a = 9;
+//          // Unitialized Variables
+//  // initialize these three variables
+//    var a = 5;
+//    var b = 10;
+//    var c = "I am a";
+
+//    //Do not change code below this line
+  
+//    a = a + 1;
+//    b = b + 5;
+//    c = c + "string!";
+            // Case Senitivity in variables 
+    
+    //Declarations
+// //  var stUdLYCapVaR; - 
+//     var studyCapVar;
+//  var properCamelCase;
+// //  var TitleCaseOver;
+// var titleCaseOver;
+
+// // Assignments
+// //  STUDLYCAPVAR = 10;
+//  studlyCapVar = 10;
+// //  PRoperCAmelCAse = "A String";
+//  properCamelCase = "A String"
+// //  tITLEcASEoVER = 9000;
+// titleCaseOver = 9000;
+//                  Adding Numbers
+//             var sum = 10 + 10;
+//             console.log(sum)
+// //                      Subracting Numbers
+//             var difference = 45 - 33;
+//             console.log()
+// //                            Multiplying Numbers
+//             var product = 8 * 10;
+// //                         Division Numbers
+//             var quotient = 66 / 33;
+
+            // Incrementing Numbers
+        // var myVar = 87;
+
+    // only change code below this line
+        // myVar = myVar + 1;
+        // myVar++
+     
+        // decrementing Numbers 
+    // var myVar = 11;
+
+    // // only change code below this line
+    // // myVar = myVar - 1;
+    //  myVar--;
+    // 
+    //              Decimal Numbers
+    
+    // var ourDecimal = 5.7;
+
+    // only change code below this line
+    
+    // var myDecimal = 0.009
+
+    //  Multiply Decimals
+    // var product = 2.0 * 2.5;
+    //    console.log(product)
+
+
+    //    var remainder;
+    //    remainder = 11 % 3;
+       //  Compound Assignment with Augmented Addition
+
+    // var a = 3;
+    // var b = 17;
+    // var c = 12;
+
+    // only modify code below this line
+    
+    // a += 12; 
+    // b += 9
+    // c += 7;
+
+    //   //  Compound Assignment with Augmented Subtraction
+//     var a = 11;
+//     var b = 9;
+//     var c = 3;
+
+//     // only modify code below this line
+    
+// a -= 6;
+// a -= 15;
+// c -= 1;
+// 
+//   //  Compound Assignment with Augmented Multiplication
+
+// var a = 5;
+// var b = 12;
+// var c = 4.6;
+
+// // only modify code below this line
+
+// a -= 5;
+// b -= 3;
+// c -= 10;
+
+
+//  Compound Assignment with Augmented Multiplication
+
+// var a = 48;
+// var b = 108;
+// var c = 33;
+
+// only modify codes below this line
+
+
+// a /=  12;
+// b /=  4;
+// c /=  11;
+
+// /       / Delclare String variables
+
+// Example
+// var firstName = "Alan";
+// var lastName  =  "Turing";
+
+//// only modify codes below this line
+
+// var myFirstName = "Beau";
+// var myLastName  = "Carnes"
+// 
+// 
+//              Escaping Literal Quotes in Strings
+
+// var myStr = "I am a \"double quoted\"string inside \"double quotes"
+// console.log(myStr)
+//          
+//         Quating Strings with single Quotes
+
+// var myStr = '<a href ="http://www.example.com" target="_blank">Link</a>';
+
+            // Escape Sequences in Strings
+            
+/*
+        CODE OUTPUT
+        \' single quote
+        \"" double quote
+        \\ backslash
+        \n newline
+        \r carriage return
+        \t tab
+        \b backspace
+        \f form feed
+*/
+// var myStr = "FirstLine\n\t\\SecondLine\nThridLine"
+// 
+//      Concatenating Strings with plus Operator
+// Example
+// var ourStr = "I come first. " + "I come second.";
+
+// // only change code below this line
+
+// var myStr = "This is start. " + "this is the end."
+// console .log(myStr)
+
+        //  Concatenating Strings with plus Equals Operator
+    // Example
+// var ourStr = "I come first. ";
+// ourStr += "I come second . ";
+
+// // only change code below this line 
+ 
+// var myStr = "This is the first sentence. "
+// myStr +=  "This is the second sentence."
+
+// console.log(myStr)
+// 
+//      Constructing Strings with Variables
+// Example
+//  var ourName = "freeCodeCamp";
+//  var ourStr  = "Hello, our name is " + ourName + ", how are you?";
+
+//  // only change code below this line
+
+//  var myName = "Beau"
+//  var myStr  = "my name is " + myName  +  " and I am well!";
+
+//  console.log(myStr)
+
+        // Appending Variables to Strings
+
+// Examples 
+// var anAjective = "awesome!";
+// var ourStr =  "freeCodeCamp is ";
+// ourStr += anAdjective;
+
+// // only change code below this line
+
+// var someAdjective = "worthwhile";
+// var myStr = "Learning to code is ";
+// myStr +=  someAdjective;
+
+        // find length of string
+// Example 
+// var firstNameLength = 0;
+// var firstName = "Ada";
+
+// firstNameLength = firstName.length;
+
+// // Setup
+// var lastNameLength = 0;
+// var lastName =  "lovelace";
+
+// // only change code below this line
+
+// lastNameLength = lastName.length;
+// console.log(lastName.length)
+// 
+//     Bracket Notation to Find First Character in String
+
+//Example
+// var firstLetterofFirstName = "";
+// var firstName = "Ada";
+
+// firstLetterofFirstName = firstName[0];
+
+// //setup
+
+// var firstLetterofLastName = "";
+// var lastName = "loveLace";
+
+// // only change code below this line
+// firstLetterofLastName = lastName[0];
+// console.log(firstLetterofLastName);
+//      String Immutability
